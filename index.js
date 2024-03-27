717b483dadbcca0fc769829043157d049468e9c9
noureenkhilji <164169970+noureenkhilji@users.noreply.github.com>
2024-03-27 17:00:15 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter your first number", type: "number", name: "firstNumber" },
+    { message: "Enter your second number", type: "number", name: "secondNumber" },
+    {
+        message: "select one of operators to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["ADDITION", "SUBTRACTION", "MULTIPLICATION", "DIVISION"],
+    },
+]);
+// CONDITIONAL STATEMENT
+if (answer.operatsctor === "ADDITION") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "SUBTRACTION") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "MULTIPLICATION") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "DIVISION") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    "Please select correct operator";
+}
